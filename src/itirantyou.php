82eb0b0caa47ba618356c0b1ec1d154b4717bd59
daiki0429 <daikinini315@gmail.com>
2024-01-15 10:59:37 +0900

    add

---
+++ src/itirantyou.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="jaq">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>音楽一覧</h2>
+    <a href="">メニューに戻る</a>
+</body>
+</html>
---
+++ src/kousin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>音楽更新</h2>
+    <a href="menyu.php">メニューに戻る</a>
+    <form action method="post">
+        "音楽ID:"
+    <input type="text" name="newsong">
+        "楽曲名"
+    <input type="text" name="newsinger">
+        "アーティスト名"
+    <input type="text" name="newsong">
+    <button type="submit" name="action" value="send">更新</button>
+</body>
+</html>
