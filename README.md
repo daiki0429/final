53dc1a6c268e89ee7c9bef0abd6c899154da70b6
daiki0429 <daikinini315@gmail.com>
2024-01-15 10:57:08 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # final
+学籍番号:2201380
+クラス：SD2G
+氏名：林　大貴
+システム名：音楽一覧システム
+URL：[Title](https://github.com/daiki0429/final)
---
+++ src/menyu.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>音楽一覧メニュー</h2>
+    <form action="menyu.php" method="post">
+    <button type="submit" name="select">一覧</button>
+    </form>
+</body>
+</html>
